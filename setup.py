132fe7c199db9d6a9e58df51939219455dbb8998
Marcus Kee <62085649+keemarcus@users.noreply.github.com>
2021-03-03 17:44:54 -0500

    fixed format error in setup page

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     task_table.drop()
     task_table = taskbook_db.create_table('task')
     task_table.insert_many([
-        {"description":"Do something useful", "date":"2021-03-03", "deadline":"2021-03-4", "list":"today", "completed":True, "prio":False, "order":0},
-        {"description":"Do something fantastic", "date":"2021-03-03", "deadline":"2021-03-4", "list":"today", "completed":False, "prio":False, "order":0},
-        {"description":"Do something remarkable", "date":"2021-03-03", "deadline":"2021-03-4", "list":"tomorrow", "completed":False, "prio":False, "order":0},
-        {"description":"Do something tomorrow", "date":"2021-03-03", "deadline":"2021-03-4", "list":"tomorrow", "completed":False, "prio":False, "order":0},
-        {"description":"Do something later", "date":"2021-03-03", "deadline":"2021-03-4", "list":"later", "completed":False, "prio":False, "order":0},
-        {"description":"Do something cool", "date":"2021-03-03", "deadline":"2021-03-4", "list":"later", "completed":True, "prio":False, "order":0},
-        {"description":"default user task", "date":"2021-03-03", "deadline":"2021-03-4", "list":"today", "completed":False, "prio":False, "order":0, "userId":1},
-        {"description":"admin task", "date":"2021-03-03", "deadline":"2021-03-4", "list":"today", "completed":False, "prio":False, "order":0, "userId":2}
+        {"description":"Do something useful", "date":"2021-03-03", "deadline":"2021-03-04", "list":"today", "completed":True, "prio":False, "order":0},
+        {"description":"Do something fantastic", "date":"2021-03-03", "deadline":"2021-03-04", "list":"today", "completed":False, "prio":False, "order":0},
+        {"description":"Do something remarkable", "date":"2021-03-03", "deadline":"2021-03-04", "list":"tomorrow", "completed":False, "prio":False, "order":0},
+        {"description":"Do something tomorrow", "date":"2021-03-03", "deadline":"2021-03-04", "list":"tomorrow", "completed":False, "prio":False, "order":0},
+        {"description":"Do something later", "date":"2021-03-03", "deadline":"2021-03-04", "list":"later", "completed":False, "prio":False, "order":0},
+        {"description":"Do something cool", "date":"2021-03-03", "deadline":"2021-03-04", "list":"later", "completed":True, "prio":False, "order":0},
+        {"description":"default user task", "date":"2021-03-03", "deadline":"2021-03-04", "list":"today", "completed":False, "prio":False, "order":0, "userId":1},
+        {"description":"admin task", "date":"2021-03-03", "deadline":"2021-03-04", "list":"today", "completed":False, "prio":False, "order":0, "userId":2}
     ])
 
     user_table = taskbook_db.get_table('user')
