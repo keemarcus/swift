267a459fd4b09873ccafd078b39b90a21342a7e9
Marcus Kee <62085649+keemarcus@users.noreply.github.com>
2021-04-15 19:21:00 -0400

    Update swift.py

--- swift.py
+++ swift.py
@@ -195,4 +195,4 @@ if PYTHONANYWHERE:
     application = default_app()
 else:
    if __name__ == "__main__":
-       run(host='0.0.0.0', port=8080, debug=True)
+       run(host=0.0.0.0, port=8080, debug=True)
